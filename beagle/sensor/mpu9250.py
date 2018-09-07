fa8e1adeb5fee6723bf75e5a93f4effe731a1de7
howeverforever <longbiau@hotmail.com.tw>
2018-09-07 19:31:22 +0800

    Correct into UTC+8 wall time.

--- beagle/sensor/mpu9250.py
+++ beagle/sensor/mpu9250.py
@@ -8,9 +8,12 @@ __all__ = ['MPU']
 
 class MPU(object):
 
-    def __init__(self, imu_bus=IMU_BUS, mpu_addr=MPU_DEFAULT_I2C_ADDR):
+    def __init__(self, imu_bus=IMU_BUS, mpu_addr=MPU_DEFAULT_I2C_ADDR, sample_rate=200):
         self._bus = smbus.SMBus(imu_bus)
         self._addr = mpu_addr
+        self._dmp_en = 1
+        self._bypass_en = 0
+        self._mag_factory_adjust = [0.0] * 3
 
         self.__reset_mpu()
         self.__check_who_am_i()
@@ -20,6 +23,8 @@ class MPU(object):
         self.__set_gyro_fsr(GYRO_FSR_2000DPS)
         self.__set_accel_dlpf(ACCEL_DLPF_184)
         self.__set_gyro_dlpf(GYRO_DLPF_184)
+        # self.__mpu_set_sample_rate(sample_rate)
+        self.__init_magnetometer()
 
         time.sleep(0.1)
 
@@ -34,10 +39,71 @@ class MPU(object):
 
         return 0
 
+    def __init_magnetometer(self):
+        if self.__mpu_set_bypass(1) < 0:
+            return -1
+
+        # Power down magnetometer
+        self._bus.write_byte_data(AK8963_ADDR, AK8963_CNTL, MAG_POWER_DN)
+        time.sleep(0.01)
+
+        #  Enter Fuse ROM access mode
+        self._bus.write_byte_data(AK8963_ADDR, AK8963_CNTL, MAG_FUSE_ROM)
+        time.sleep(0.01)
+
+        raw = self._bus.read_i2c_block_data(AK8963_ADDR, AK8963_ASAX, 3)
+
+        # Return sensitivity adjustment values
+        self._mag_factory_adjust[0] = (raw[0] - 128) / 256.0 + 1.0
+        self._mag_factory_adjust[1] = (raw[1] - 128) / 256.0 + 1.0
+        self._mag_factory_adjust[2] = (raw[2] - 128) / 256.0 + 1.0
+
+        # Power down magnetometer again
+        self._bus.write_byte_data(AK8963_ADDR, AK8963_CNTL, MAG_POWER_DN)
+        time.sleep(0.01)
+
+        # Configure the magnetometer for 16 bit resolution
+        # and continuous sampling mode 2 (100hz)
+        c = MSCALE_16 | MAG_CONT_MES_2
+        self._bus.write_byte_data(AK8963_ADDR, AK8963_CNTL, c)
+        time.sleep(0.01)
+
+        return 0
+
+    def __mpu_set_bypass(self, bypass_on):
+        tmp = 0
+        if self._dmp_en > 0:
+            tmp |= FIFO_EN_BIT
+        if bypass_on == 0:
+            tmp |= I2C_MST_EN
+        self._bus.write_byte_data(self._addr, USER_CTRL, tmp)
+
+        time.sleep(0.03)
+
+        # INT_PIN_CFG settings
+        tmp = LATCH_INT_EN | INT_ANYRD_CLEAR | ACTL_ACTIVE_LOW
+        if bypass_on > 0:
+            tmp |= BYPASS_EN
+        self._bus.write_byte_data(self._addr, INT_PIN_CFG, tmp)
+        if bypass_on > 0:
+            self._bypass_en = 1
+        else:
+            self._bypass_en = 0
+
+        return 0
+
     def __check_who_am_i(self):
         c = self._bus.read_byte_data(self._addr, 0)
         if c != 0x71:
             return -1
+
+        return 0
+
+    def __mpu_set_sample_rate(self, rate):
+        if rate > 1000 or rate < 4:
+            raise ValueError('sample rate must be between 4 & 1000')
+        div = int(np.uint8(1000 / rate - 1))
+        self._bus.write_byte_data(self._addr, SMPLRT_DIV, div)
 
         return 0
 
@@ -166,3 +232,22 @@ class MPU(object):
         gz = round(gz, 4)
 
         return {'gx': gx, 'gy': gy, 'gz': gz}
+
+    def mpu_read_mag(self):
+        raw = self._bus.read_i2c_block_data(AK8963_ADDR, AK8963_XOUT_L, 6)
+
+        # Turn the MSB and LSB into a signed 16-bit value
+        # Data stored as little Endian
+        mx_raw = np.int16(np.int16(raw[0] << 8) | raw[1])
+        my_raw = np.int16(np.int16(raw[2] << 8) | raw[3])
+        mz_raw = np.int16(np.int16(raw[4] << 8) | raw[5])
+
+        # multiply by the sensitivity adjustment and convert to units of uT micro
+        # Teslas. Also correct the coordinate system as someone in InvenSense
+        # thought it would be bright idea to have the magnetometer coordinate
+        # system aligned differently than the accelerometer and gyro.... -__-
+        mx = my_raw * self._mag_factory_adjust[1] * MAG_RAW_TO_uT
+        my = mx_raw * self._mag_factory_adjust[0] * MAG_RAW_TO_uT
+        mz = -mz_raw * self._mag_factory_adjust[2] * MAG_RAW_TO_uT
+
+        return {'mx': mx, 'my': my, 'mz': mz}
--- read6axis.py
+++ read6axis.py
@@ -9,23 +9,29 @@ def main():
     mpu = bg.MPU()
 
     ts = time.time()
-    st = datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H%M%S')
+    st = bg.get_datetime(ts).strftime('%Y-%m-%d_%H%M%S')
 
+    print(st)
     try:
         with open(st + '.csv', 'w') as f:
             writer = csv.writer(f)
-            writer.writerow(['timestamp', 'imu_ax', 'imu_ay', 'imu_az', 'imu_gx', 'imu_gy', 'imu_gz'])
+            writer.writerow(['timestamp',
+                             'imu_ax', 'imu_ay', 'imu_az',
+                             'imu_gx', 'imu_gy', 'imu_gz',
+                             'imu_mx', 'imu_my', 'imu_mz'])
             while True:
                 try:
                     ts = time.time()
                     accel = mpu.mpu_read_accel()
                     gyro = mpu.mpu_read_gyro()
+                    mag = mpu.mpu_read_mag()
                     row = [ts]
                     row += [accel['ax'], accel['ay'], accel['az']]
                     row += [gyro['gx'], gyro['gy'], gyro['gz']]
+                    row += [mag['mx'], mag['my'], mag['mz']]
                     writer.writerow(row)
 
-                    time.sleep(0.01)
+                    time.sleep(0.005)
                 except KeyboardInterrupt:
                     f.close()
                     break
