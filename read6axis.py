a856a6ed6aec226975dc0414f7cde575ca5fbc6e
howeverforever <longbiau@hotmail.com.tw>
2018-09-07 16:43:55 +0800

    Add timestamp column.

--- read6axis.py
+++ read6axis.py
@@ -2,23 +2,30 @@ import beagle as bg
 import sys
 import time
 import csv
+from datetime import datetime
 
 
 def main():
     mpu = bg.MPU()
 
+    ts = time.time()
+    st = datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H%M%S')
+
     try:
-        with open('output.csv', 'w') as f:
-            writer = csv.writer(f, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(['imu_ax', 'imu_ay', 'imu_az', 'imu_gx', 'imu_gy', 'imu_gz'])
+        with open(st + '.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['timestamp', 'imu_ax', 'imu_ay', 'imu_az', 'imu_gx', 'imu_gy', 'imu_gz'])
             while True:
                 try:
+                    ts = time.time()
                     accel = mpu.mpu_read_accel()
                     gyro = mpu.mpu_read_gyro()
-                    row = [accel['ax'], accel['ay'], accel['az']]
+                    row = [ts]
+                    row += [accel['ax'], accel['ay'], accel['az']]
                     row += [gyro['gx'], gyro['gy'], gyro['gz']]
                     writer.writerow(row)
 
+                    time.sleep(0.01)
                 except KeyboardInterrupt:
                     f.close()
                     break
